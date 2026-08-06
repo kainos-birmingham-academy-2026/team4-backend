bf9ce836aa44b9531c272b1a14cdbe412ecce2c2
Asfur Malik <asfur.malik@kainos.com>
2026-08-06 13:12:09 +0100

    Add separate file for mock data

---
+++ tests/mockJobRoles.ts
@@ -0,0 +1,16 @@
+export const mockJobRole1 = {
+    id: 1,
+    title: "Software Engineer",
+    description: "Develop and maintain software applications.",
+    location: "Remote",
+    isOpen: true
+};
+
+export const mockJobRole2 = {
+    id: 2,
+    title: "Product Manager",
+    description: "Oversee product development and strategy.",
+    location: "New York",
+    isOpen: true
+};
+export const mockJobRoles = [mockJobRole1, mockJobRole2];
