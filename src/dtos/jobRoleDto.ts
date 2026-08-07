2a08ca2f8486aadb2f3186267b06a68f3582e13e
Kazi Arham <kaziarham786@gmail.com>
2026-08-07 15:23:33 +0100

    Made slight changes to make the closingDate field be null

--- src/dtos/jobRoleDto.ts
+++ src/dtos/jobRoleDto.ts
@@ -4,10 +4,10 @@ export class JobRoleResponse {
         public readonly location: string,
         public readonly capability: string,
         public readonly band: string,
-        public readonly closingDate: Date,
+        public readonly closingDate: Date | null,
     ) {
-        if (!roleName || !location || !capability || !band || !closingDate) {
-            throw new Error("All fields are required");
+        if (!roleName || !location || !capability || !band) {
+            throw new Error("Role name, location, capability, and band fields are required");
         }
     }
 }
