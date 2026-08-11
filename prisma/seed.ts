13057a7f1ef5c7ff79f36a0cc2efaac244693798
Kazi Arham <kaziarham786@gmail.com>
2026-08-11 10:24:32 +0100

    Added longer description and actual Sharepoint URL to seeded data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -81,9 +81,9 @@ async function main() {
                 bandId: bandMap.Trainee,
                 closingDate: new Date("2026-10-22"),
                 statusId: statusMap.Open,
-                description: "Responsible for developing and maintaining software applications.",
+                description: "As a Graduate Software Engineer with Kainos, you will work on projects where you can make a real difference to people's lives - the lives of people you know. After taking part in our award-winning, seven-week Engineering Academy, you will then join one of our many project teams, to learn from our experienced developers, project managers and customer-facing staff. You'll have great support and mentoring, balanced with the experience of being given real, meaningful work to do, to help you truly develop both technically and professionally.",
                 responsibilities: ["Write code", "Fix bugs", "Collaborate with team"],
-                sharepointUrl: "https://example.com/graduate-software-engineer",
+                sharepointUrl: "https://kainossoftwareltd.sharepoint.com/sites/Career/JobProfiles/Engineering/Job%20profile%20-%20Software%20Engineer%20(Trainee).pdf",
                 numberOfOpenPositions: 5
             },
             {
@@ -93,9 +93,9 @@ async function main() {
                 bandId: bandMap["Senior Associate"],
                 closingDate: new Date("2026-09-19"),
                 statusId: statusMap.Open,
-                description: "Responsible for leading test engineering efforts.",
+                description: "As a Senior Test Engineer (Senior Associate) in Kainos, you'll work within a multi-skilled agile team, developing and executing functional automated and manual tests to help the team deliver working application software that meets user needs. You'll do this whilst learning about new technologies and approaches, with talented colleagues who will help you learn, develop and grow.",
                 responsibilities: ["Design test plans", "Execute test cases", "Report defects"],
-                sharepointUrl: "https://example.com/senior-test-engineer",
+                sharepointUrl: "https://kainossoftwareltd.sharepoint.com/sites/Career/JobProfiles/Engineering/Job%20profile%20-%20Senior%20Test%20Engineer%20(SA).pdf",
                 numberOfOpenPositions: 2
             },
             {
@@ -105,9 +105,9 @@ async function main() {
                 bandId: bandMap.Associate,
                 closingDate: new Date("2026-10-13"),
                 statusId: statusMap.Open,
-                description: "Responsible for supporting platform engineering tasks.",
+                description: "As Platform Engineer (Associate) in Kainos, you'll be responsible for automating, building and supporting modern digital service platforms using public cloud technology. You'll be continually learning about new technologies, approaches and industry best practices all whilst being mentored and coached by talented colleagues who will help you learn, develop and grow. Successful candidates will have a passion for technology and a thirst for learning.",
                 responsibilities: ["Maintain platforms", "Support platform upgrades", "Monitor platform performance"],
-                sharepointUrl: "https://example.com/associate-platform-engineer",
+                sharepointUrl: "https://kainossoftwareltd.sharepoint.com/sites/Career/JobProfiles/Platforms/Job%20profile%20-%20Platform%20Engineer%20(Associate).pdf",
                 numberOfOpenPositions: 3
             },
             {
@@ -117,9 +117,9 @@ async function main() {
                 bandId: bandMap.Consultant,
                 closingDate: new Date("2026-12-16"),
                 statusId: statusMap.Closed,
-                description: "Responsible for leading data engineering initiatives.",
+                description: "As a Lead Data Engineer (Consultant) at Kainos you will be responsible for designing and developing data processing and data persistence software components for solutions which handle data at scale. Working in agile teams, Lead Data Engineers providing strong development leadership for team members and take responsibility for the quality of the codebase as well as the match to user needs.",
                 responsibilities: ["Design data pipelines", "Implement data solutions", "Mentor junior engineers"],
-                sharepointUrl: "https://example.com/lead-data-engineer",
+                sharepointUrl: "https://kainossoftwareltd.sharepoint.com/sites/Career/JobProfiles/Forms/AllItems.aspx?id=%2Fsites%2FCareer%2FJobProfiles%2FData%20and%20Artificial%20Intelligence%2FArchive%2FJob%20specification%20%2D%20Lead%20Data%20Engineer%20%28C%29%2Epdf&parent=%2Fsites%2FCareer%2FJobProfiles%2FData%20and%20Artificial%20Intelligence%2FArchive",
                 numberOfOpenPositions: 0
             },
             {
@@ -129,9 +129,9 @@ async function main() {
                 bandId: bandMap.Manager,
                 closingDate: new Date("2026-11-30"),
                 statusId: statusMap.Open,
-                description: "Responsible for managing low code solutions.",
+                description: "As a Low Code Solution Architect (Manager) in Kainos, you'll be responsible for managing multi-skilled delivery teams to design and deliver high Low Code solutions which delight our customers and impact the lives of users worldwide. You'll work with customer architects to agree functional and non-functional designs, advising customers and managers on the estimated effort, technical implications and complexity surrounding your designs. You'll manage, coach and develop a small number of staff, with a focus on managing employee performance and assisting in their career development. You'll also provide direction for your team as you solve challenging problems together. You will work with your peers to develop policy and standards, share knowledge and mentor those around you. You'll do this whilst advising about new technologies and approaches, with room to learn, develop and grow.",
                 responsibilities: ["Design low code solutions", "Implement low code solutions", "Mentor junior engineers"],
-                sharepointUrl: "https://example.com/low-code-solution-architect",
+                sharepointUrl: "https://kainossoftwareltd.sharepoint.com/sites/Career/JobProfiles/Engineering/Job%20profile%20-%20Low%20Code%20Solution%20Architect%20(M)%20.pdf",
                 numberOfOpenPositions: 5
             },
             {
@@ -141,9 +141,9 @@ async function main() {
                 bandId: bandMap["Senior Associate"],
                 closingDate: new Date("2026-10-15"),
                 statusId: statusMap.Open,
-                description: "Responsible for talent acquisition and recruitment.",
+                description: "The Talent Acquisition team at Kainos are responsible for end to end delivery aligned to BU hiring demands. Based in locations across UK &I, Europe and North America, the team focus on direct sourcing channels to create a best in class candidate experience, in the most cost-effective way. As a Talent Acquisition Partner (Senior Associate), you will work closely with team members to manage the attraction and acquisition of high calibre talent. Providing best in class candidate experience and acting as a brand ambassador for recruitment at Kainos internally and externally.",
                 responsibilities: ["Source candidates", "Conduct interviews", "Manage recruitment process"],
-                sharepointUrl: "https://example.com/talent-acquisition-partner",
+                sharepointUrl: "https://kainossoftwareltd.sharepoint.com/sites/Career/JobProfiles/People/Talent%20Acquisition/Job%20Profile%20-%20Talent%20Acquisition%20Partner%20(SA).pdf",
                 numberOfOpenPositions: 2
             }
         ],
