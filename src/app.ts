4771e4dd087046b0f8a3bce846a9d4f556e8344a
Dane-W-K <dane.westley@kainos.com>
2026-08-13 09:46:16 +0100

    Added chatbot + role filtering (#13)
    * Added chatbot + role filtering
    
    * Fixed bugs with connectivity and display
    
    * Updated tests
    
    * Added more coverage

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import authRouter from "./routes/authRouter.js";
+import chatRouter from "./routes/chatRouter.js";
 import jobRoleRouter from "./routes/jobRoleRouter.js";
 
 export const app = express();
@@ -7,4 +8,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/job-roles", jobRoleRouter);
+app.use("/api/chat", chatRouter);
 app.use("/auth", authRouter);
