a8a0af2ddfbf854a72db4128b0849ed6f27b78ad
Sean Vial <sean.vial@kainos.com>
2026-08-26 15:06:02 +0100

    removed role assignment for error fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,6 @@ module "key_vault" {
   location            = module.resource_group.location
   environment         = var.environment
   tenant_id           = data.azurerm_client_config.current.tenant_id
-  admin_object_id     = data.azurerm_client_config.current.object_id
 }
 
 moved {
