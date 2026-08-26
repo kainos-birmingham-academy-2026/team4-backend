01e31138694fa644680a62e48352016dabdc7c93
SeanV411 <sean.vial@kainos.com>
2026-08-26 14:40:38 +0100

    Made key vault (#23)

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,17 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      # Dev-friendly: destroy can purge a soft-deleted vault so the name can be reused.
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
 }
+
+# Who is running Terraform (your user locally, later the GitHub Actions OIDC identity).
+data "azurerm_client_config" "current" {}
 
 module "resource_group" {
   source = "./modules/resource-group"
@@ -25,6 +34,17 @@ module "resource_group" {
   name        = var.resource_group_name
   location    = var.location
   environment = var.environment
+}
+
+module "key_vault" {
+  source = "./modules/key-vault"
+
+  name                = var.key_vault_name
+  resource_group_name = module.resource_group.name
+  location            = module.resource_group.location
+  environment         = var.environment
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  admin_object_id     = data.azurerm_client_config.current.object_id
 }
 
 moved {
